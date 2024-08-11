213f5183cdd94bcff8858b607c8bc708eac564bf
Lahiru Dilshan <159991364+lahirudilshan2@users.noreply.github.com>
2024-08-12 00:37:35 +0530

    change

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,22 @@
 <?php
     include "header.php"
 ?>
-<div>
-    
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<section id="contact">
+        <!-- <h1 class="heading1">Contact Me</h1> -->
+        <form action="" class="form">
+            <input type="text" name="name" class="input" placeholder="Enter Your Name">
+            <input type="email" name="email" class="input" placeholder="Enter Your Email">
+            <input type="msg" name="msg" id="msg" placeholder="Enter Your Massage">
+            <input type="submit" value="SEND" id="send"> 
+        </form>
+    </section>
+</body>
+</html>
